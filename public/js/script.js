f34e493f66838efe2fa8767677016f61e4493160
Michal Gorecki <michal.gorecki@itsense.pl>
2021-01-20 21:15:25 +0100

    poprawka na ECMAscripts

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,18 @@
-function openModal() {
+const openModal = () => {
     document.getElementById("backdrop").style.display = "block";
     document.getElementById("exampleModal").style.display = "block";
     document.getElementById("exampleModal").className += " show fade";
 }
 
-function closeModal() {
-    document.getElementById("backdrop").style.display = "none";
-    document.getElementById("exampleModal").style.display = "none";
-    document.getElementById("exampleModal").className = "modal fade";
-}
-// Get the modal
-var modal = document.getElementById('exampleModal');
+const closeModal = () => {
+        document.getElementById("backdrop").style.display = "none";
+        document.getElementById("exampleModal").style.display = "none";
+        document.getElementById("exampleModal").className = "modal fade";
+    }
+    // Get the modal
+let modal = document.getElementById('exampleModal');
 
-function addWorker() {
+const addWorker = () => {
     document.getElementById("workers").innerHTML += '<p style="margin-top:10px; margin-bottom:2px">Dane pracownika</p><input type="text" class="col-md-6" style="padding:5px; margin-bottom:3px; " placeholder="Imię" name="forename[]" required><input type="text" class="col-md-6" style="padding:5px; margin-bottom:3px" placeholder="Nazwisko" name="surname[]" required><input type="text" class="col-md-6" style="padding:5px; margin-bottom:3px" placeholder="Email" name="email[]" required><input type="text" class="col-md-6" style="padding:5px; margin-bottom:3px" placeholder="Telefon" name="phone[]">';
 }
 
